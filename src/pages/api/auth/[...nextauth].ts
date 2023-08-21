e6a82e8dd283a1a609d14f8a7beefd0d990b9991
Rian Landim <rianlandim@MacBook-Air-de-Rian.local>
2023-08-21 20:52:57 -0300

    first commit

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,68 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import bcrypt from "bcrypt";
+
+import { prisma } from "../../../server/db";
+
+export const authOptions: NextAuthOptions = {
+    callbacks: {
+        session({ session, token, user }) {
+            if (session.user) {
+                session.user.id = token.sub ?? "";
+            }
+
+            return session;
+        },
+        jwt({ token }) {
+
+            return token;
+        },
+    },
+    adapter: PrismaAdapter(prisma),
+    // Configure one or more authentication providers
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                // Add logic here to look up the user from the credentials supplied
+                const user = await prisma.user.findFirst({
+                    where: {
+                        email: credentials?.email,
+                    },
+                });
+
+                if (!user) {
+                    return null;
+                }
+
+                const password = credentials?.password ?? "";
+                const userPassword = user.password ?? "";
+
+                const passwordMatches = await bcrypt.compare(password, userPassword);
+
+                if (!passwordMatches) {
+                    return null;
+                }
+
+                return {
+                    id: user.id,
+                };
+            },
+            type: "credentials",
+        }),
+    ],
+    session: {
+        strategy: "jwt",
+    },
+    pages: {
+        signIn: "/login",
+    },
+};
+
+export default NextAuth(authOptions);
