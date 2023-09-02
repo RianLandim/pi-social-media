3edaca8517d0a00149ee8c3461628f4f18620675
Rian Landim <rianlandim@MacBook-Air-de-Rian.local>
2023-09-02 16:37:16 -0300

    created the registration form

--- src/pages/auth/registre.tsx
+++ src/pages/auth/registre.tsx
@@ -1,4 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Eye } from "@phosphor-icons/react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import { api } from "~/utils/api";
@@ -12,11 +13,9 @@ const userFormSchema = z.object({
 type UserFormSchemaProps = z.infer<typeof userFormSchema>;
 
 export default function Register() {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<UserFormSchemaProps>({ resolver: zodResolver(userFormSchema) });
+  const { register, handleSubmit } = useForm<UserFormSchemaProps>({
+    resolver: zodResolver(userFormSchema),
+  });
 
   const createUserMutation = api.user.create.useMutation();
 
@@ -24,31 +23,33 @@ export default function Register() {
     createUserMutation.mutate(data);
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-slate-400 p-8">
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-slate-400 p-8">
+      <h1 className="text-2xl font-bold uppercase text-white">Registre-se</h1>
       <form
         onSubmit={handleSubmit(submit)}
-        className="flex flex-col items-center justify-center space-y-4"
+        className="flex flex-col items-center justify-center space-y-4 rounded-md bg-slate-200 p-8"
       >
         <div>
           <label>Nome</label>
           <input
             {...register("name")}
-            className="w-full rounded-md bg-slate-600 p-2"
+            className="w-full rounded-md bg-white p-2"
           />
         </div>
         <div>
           <label>Email</label>
           <input
             {...register("email")}
-            className="w-full rounded-md bg-slate-600 p-2"
+            className="w-full rounded-md bg-white p-2"
           />
         </div>
         <div>
           <label>Senha</label>
           <input
             {...register("password")}
-            className="w-full rounded-md bg-slate-600 p-2"
-          />
+            className="w-full rounded-md bg-white p-2"
+            type="password"
+          ></input>
         </div>
         <button type="submit" className="w-full rounded-md bg-blue-500 p-2">
           Cadastrar
