647957f4f1d046670939ad0a388542b06cb7d0ed
Wesley Kelvy <lilwkelvy@gmail.com>
2023-09-24 15:07:22 -0300

    PISD-35: added html aside

---
+++ src/pages/auth/aside.tsx
@@ -0,0 +1,35 @@
+export default function Aside() {
+  
+  return (
+    <div id="container__menu-lateral">
+      <div id="logo">
+        <h1>LOGO</h1>
+      </div>
+
+      <div id="box__user-infos">
+          <div id="user-photo"></div>
+          <div id="user-texts">
+            <div id="user-name">
+              <p>John Cena</p>
+            </div>
+            <div id="user-atSign">
+              <p>@xxxxxx_xx</p>
+            </div>
+          </div>
+        </div>
+        <div id="menu-lateral__list">
+          <ul id="list-btn">
+            <li>Página Inicial</li>
+            <li>Notificações</li>
+            <li>Mensagens</li>
+            <li>Salvos</li>
+            <li>Configurações</li>
+          </ul>
+        </div>
+      </div>
+
+      
+    
+
+  )
+}
