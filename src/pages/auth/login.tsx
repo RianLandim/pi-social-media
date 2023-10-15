4e5f484e391ce5f9fc262626b8e7f479806ab8bb
Eduardo Araújo <eduardoaraujomilfont@gmail.com>
2023-10-15 17:28:05 -0300

    login bugfix

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -36,39 +36,52 @@ export default function Login() {
   };
 
   return (
-    <main className={'h-screen bg-slate-800 w-screen flex items-center justify-center font-poppins'}>
+    <main
+      className={
+        "font-poppins flex h-screen w-screen items-center justify-center bg-slate-800"
+      }
+    >
       <form
-        className="p-12 bg-black items-center flex flex-col space-y-4 w-96 h-2/3 rounded-lg">
-        <img className="rounded-md" src="https://i.imgur.com/BhWGMn1.jpg" alt="" />
+        onClick={handleSubmit(submit)}
+        className="flex h-2/3 w-96 flex-col items-center space-y-4 rounded-lg bg-black p-12"
+      >
+        <img
+          className="rounded-md"
+          src="https://i.imgur.com/BhWGMn1.jpg"
+          alt=""
+        />
         <div className="flex flex-col space-y-4">
-        <h3 className="text-3xl text-white font-bold">Entrar em Paperland</h3>
-        <div className="w-full flex flex-row bg-black items-center border border-[#8f8f8f] rounded-md p-1">
-          <input
-            className="w-full p-1 bg-black text-white"
-            placeholder="Email"
-            type="email"
-          />
-
-        </div>
-        <div className="w-full flex flex-row bg-black items-center border border-[#8f8f8f] rounded-md p-1">
-          <input
-            className="w-full p-1 rounded-md bg-black"
-            placeholder="Senha"
-            type='password'
-          />
-        </div>
-        <div className="flex flex-col space-y-2 items-center">
-        <button className="w-52 p-2 bg-[#B20DFF] rounded-md hover:bg-[#f719be]" >
-          <span className="text-white font-bold">Entrar</span>
-        </button>
-        <button className="w-52 p-2 bg- rounded-md border-solid border hover:bg-[#333333]" >
-          <span className="text-white font-bold">Esqueceu a senha?</span>
-        </button>
-        <span className="text-white"> 
-          Criar conta? <a className="text-blue-400" href="">Inscreva-se</a> 
-        </span>
-        </div>
-        
+          <h3 className="text-3xl font-bold text-white">Entrar em Paperland</h3>
+          <div className="flex w-full flex-row items-center rounded-md border border-[#8f8f8f] bg-black p-1">
+            <input
+              className="w-full bg-black p-1 text-white"
+              placeholder="Email"
+              type="email"
+              {...register("email")}
+            />
+          </div>
+          <div className="flex w-full flex-row items-center rounded-md border border-[#8f8f8f] bg-black p-1">
+            <input
+              className="w-full rounded-md bg-black p-1"
+              placeholder="Senha"
+              type="password"
+              {...register("password")}
+            />
+          </div>
+          <div className="flex flex-col items-center space-y-2">
+            <button className="w-52 rounded-md bg-[#B20DFF] p-2 hover:bg-[#f719be]">
+              <span className="font-bold text-white">Entrar</span>
+            </button>
+            <button className="bg- w-52 rounded-md border border-solid p-2 hover:bg-[#333333]">
+              <span className="font-bold text-white">Esqueceu a senha?</span>
+            </button>
+            <span className="text-white">
+              Criar conta?{" "}
+              <a className="text-blue-400" href="">
+                Inscreva-se
+              </a>
+            </span>
+          </div>
         </div>
       </form>
     </main>
