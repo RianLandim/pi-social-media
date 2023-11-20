5b9f56ab2a2a2d7838bb3c2108d59793e2d5bd76
Rian Landim <rianlandim222@gmail.com>
2023-11-20 10:58:49 -0300

    added the replication to mariadb

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,44 @@ import { useSession } from "next-auth/react";
 import { api } from "~/utils/api";
 import { InputPost } from "~/components/InputPost";
 import { match } from "ts-pattern";
-import LoadingIndicator from "~/components/ui/LoadingIndicator";
+
 import { NextPageWithLayout } from "./_app";
 import { getMainLayout } from "~/layout/MainLayout";
 import PostModel from "~/components/PostModel";
+import autoAnimate from "@formkit/auto-animate";
+import { useEffect, useRef } from "react";
+import { Skeleton } from "~/components/ui/skeleton";
 
 const Feed: NextPageWithLayout = () => {
   useSession({ required: true });
+
+  const parent = useRef(null);
+
+  useEffect(() => {
+    parent.current &&
+      autoAnimate(parent.current, {
+        duration: 650,
+        easing: "ease-out",
+      });
+  }, [parent]);
 
   const postsQuery = api.post.list.useQuery();
 
   return (
     <>
       <InputPost />
-      <div className="flex w-full flex-col items-center justify-center">
+      <div
+        ref={parent}
+        className="flex w-full flex-col items-center justify-center"
+      >
         {match(postsQuery)
-          .with({ isLoading: true }, () => <LoadingIndicator />)
+          .with({ isLoading: true }, () =>
+            new Array(5).map(() => (
+              <ul className="flex w-3/4 flex-col items-center justify-center p-4">
+                <Skeleton className="h-[31rem] w-full rounded-md" />
+              </ul>
+            ))
+          )
           .with({ isError: true }, () => (
             <span className="text-2xl font-bold">Erro ao carregar posts</span>
           ))
